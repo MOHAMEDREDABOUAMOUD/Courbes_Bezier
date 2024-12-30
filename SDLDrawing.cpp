c4f999acc02218853f91117516622b1e5807b0ad
Mehdi <mehdisahnou@gmail.com>
2024-12-30 13:56:55 +0100

    Final

--- SDLDrawing.cpp
+++ SDLDrawing.cpp
@@ -5,17 +5,6 @@
 // Variable globale indiquant si les fenêtres doivent continuer à s'exécuter
 bool running = true;
 
-PixelGrid SDLDrawing::Window(int x, int width, int height) {
-    PixelGrid table(width, height);
-    LetterSelector::selectLetter(table, x);
-    std::cout << "En cours..." << std::endl;
-
-    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-        std::cout << "Échec de l'initialisation de SDL" << std::endl;
-    }
-
-    return table;
-}
 
 void SDLDrawing::DrawWindow(const char* title, PixelGrid table, void (SDLDrawing::*drawFunction)(SDL_Renderer*, int**, int, int, PixelGrid)) {
     SDL_Window* window = SDL_CreateWindow(title, 
@@ -87,7 +76,15 @@ void SDLDrawing::RedLetter(SDL_Renderer* renderer, int** letterGrid, int j, int 
     }
 }
 
-void SDLDrawing::Draw(PixelGrid table) {
+void SDLDrawing::Draw(int x, int width, int height) {
+    PixelGrid table(width, height);
+    LetterSelector::selectLetter(table, x);
+    std::cout << "En cours..." << std::endl;
+
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        std::cout << "Échec de l'initialisation de SDL" << std::endl;
+    }
+
     std::thread emptyThread(&SDLDrawing::DrawWindow, this, "Police Vide", table, &SDLDrawing::EmptyLetter);
     std::thread solidThread(&SDLDrawing::DrawWindow, this, "Police Noire", table, &SDLDrawing::SolidBlackLetter);
     std::thread redThread(&SDLDrawing::DrawWindow, this, "Police Rouge", table, &SDLDrawing::RedLetter);
@@ -98,5 +95,102 @@ void SDLDrawing::Draw(PixelGrid table) {
 
     std::cout << "Toutes les fenêtres ont été fermées." << std::endl;
 
+    SDL_Quit();
+}
+
+void SDLDrawing::Draw2(int width, int height) {
+    PixelGrid table(width, height);
+
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        std::cerr << "Failed to initialize SDL: " << SDL_GetError() << std::endl;
+        return;
+    }
+
+    SDL_Window* window = SDL_CreateWindow("All letters",
+                                          SDL_WINDOWPOS_CENTERED,
+                                          SDL_WINDOWPOS_CENTERED,
+                                          table.get_width() * 7, table.get_height() * 7,
+                                          SDL_WINDOW_SHOWN);
+
+    if (!window) {
+        std::cerr << "Failed to create window: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return;
+    }
+
+    // Create SDL renderer
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (!renderer) {
+        std::cerr << "Failed to create renderer: " << SDL_GetError() << std::endl;
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return;
+    }
+
+    // Main loop variables
+    bool running = true;
+    SDL_Event event;
+    Uint32 lastTime = SDL_GetTicks(); // Time of the last action
+    const int intervalMs = 500;      // time interval
+
+    char letter =  'A';
+    int count = 0;
+    
+        
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_RenderClear(renderer);
+
+    while (running) {
+        // Handle events
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = false; // Arrêt global si la croix est cliquée
+            }
+            if (event.type == SDL_KEYDOWN && (event.key.keysym.sym == SDLK_q)) {
+                running = false; // Arrêt global si  'q' est pressé
+            }
+        }
+
+        // Get the current time
+        Uint32 currentTime = SDL_GetTicks();
+
+        if (currentTime > lastTime + intervalMs) {
+            SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+            SDL_RenderClear(renderer);
+            lastTime = currentTime;
+            LetterSelector::selectLetter(table, letter);
+            int** letterGrid = table.get_grid();
+            for (int i = 0; i < table.get_height(); i++) {
+                for (int j = 0; j < table.get_width(); j++) {
+                    if(count == 0)
+                        EmptyLetter(renderer, letterGrid, j, i, table);
+                    else if(count == 1) 
+                        SolidBlackLetter(renderer, letterGrid, j, i, table);
+                    else
+                        RedLetter(renderer, letterGrid, j, i, table);
+                    table.set_pixel(j, i, 0);///to reset the table for next iteration
+                }
+            }
+            count++;
+            if(count == 3){
+                if(letter == 'Z')
+                    letter = 'A';
+                else
+                    letter++;
+                count = 0;
+            }
+            SDL_RenderPresent(renderer);
+        }
+
+        // Update the screen
+        SDL_RenderPresent(renderer);
+
+        // Frame rate limiting
+        SDL_Delay(16); // ~60 FPS
+    }
+
+    // Cleanup
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
     SDL_Quit();
 }
--- main.cpp
+++ main.cpp
@@ -7,35 +7,33 @@
 #include "SDLDrawing.h"
 
 /**
- * @brief Fonction principale représentant le point d'entrée de l'application SDL.
+ * @brief Main function representing the entry point of the SDL application.
  * 
- * Cette fonction initialise l'objet SDLDrawing, crée une fenêtre SDL, dessine une lettre 
- * dans trois fenêtres différentes de manière simultanée grâce aux threads et gère le flux
- * de l'application.
+ * This function initializes the SDLDrawing object, creates an SDL window, 
+ * draws a letter in three different windows simultaneously using threads using lettreSDL.Draw
+ * or draws all letters one after the other in one window using lettreSDL.Draw2
+ *  
  * 
- * @param argc Nombre d'arguments en ligne de commande.
- * @param argv Tableau des arguments en ligne de commande.
- * @return Entier indiquant le statut de sortie de l'application.
+ * @param argc Number of command-line arguments.
+ * @param argv Array of command-line arguments.
+ * @return Integer indicating the application's exit status.
  */
 int SDL_main(int argc, char* argv[]) {
     try {
         // Create an SDLDrawing object
         SDLDrawing lettreSDL;
 
-        // Create a PixelGrid for the letter 'Q' with specified dimensions
-        PixelGrid table = lettreSDL.Window('Q', 40, 50);
-        if (table.get_width() == 0 || table.get_height() == 0) {
-            std::cerr << "Erreur lors de la création du PixelGrid" << std::endl;
-            return 1;
-        }
-
+        std::cout << "Click Q in any window to exit." << std::endl;
         // Draw the letter in three separate windows using threads
-        lettreSDL.Draw(table);
+        lettreSDL.Draw('Q', 40, 50);
+
+        // Draw all the letters one after another in one window
+        // lettreSDL.Draw2(40, 50);
 
         return 0;
     }
     catch (const std::exception& e) {
-        std::cerr << "Une erreur est survenue : " << e.what() << std::endl;
+        std::cerr << "An error occurred: " << e.what() << std::endl;
         return 1;
     }
 }
